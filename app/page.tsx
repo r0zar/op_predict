77c183da709644a108ee9949a914b15aad6a5141
Ross Ragsdale <ross@pointblankdev.com>
2025-03-07 14:23:18 -0600

    :art: Update

--- app/page.tsx
+++ app/page.tsx
@@ -62,7 +62,7 @@ export default async function Home() {
               <div className="flex flex-col justify-center space-y-4">
                 <div className="space-y-2 mb-4">
                   <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
-                    Predict the Future. <div className="text-primary">Earn Bitcoin.</div>
+                    Predict & <div className="text-primary">Earn Bitcoin.</div>
                   </h1>
                   <p className="max-w-[600px] text-muted-foreground md:text-lg">
                     Explore and participate in markets for politics, sports, and more.
